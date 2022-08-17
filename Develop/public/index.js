353e3deffc7a9227b988748c118c8dfb63e10740
wjimh314 <wjimh314@gmail.com>
2022-08-17 13:58:12 -0400

    pushing to heroku

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3001
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
