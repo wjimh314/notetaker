b5b7a4145c650ca8a249bbee72c84b16990af5ba
wjimh314 <wjimh314@gmail.com>
2022-08-17 15:50:04 -0400

    added two new routes

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -7,6 +7,14 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", (req, res) => {
+	res.sendfile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
