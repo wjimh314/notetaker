0ab08ac1127dc0b183558ab4013e84ba6a246e0f
wjimh314 <wjimh314@gmail.com>
2022-08-18 09:02:06 -0400

    added server file

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,12 +1,3 @@
-const fs = require("fs");
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = process.env.PORT || 3001;
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.get("/notes", (req, res) => {
 	res.sendfile(path.join(__dirname, "/public/notes.html"));
 });
