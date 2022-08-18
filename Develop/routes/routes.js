a359a68c99034715c9bcef6bfe2f4719242287f5
wjimh314 <wjimh314@gmail.com>
2022-08-18 13:18:51 -0400

    add more to server.js

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,28 +1,26 @@
-const fs= require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-module.exports = app => {
-    fs.readFile("db/db.json","utf8", (err, data) => {
-        if (err) throw err;
-        var notes = JSON.parse(data);
-        
-        app.get("/api/notes", function(req, res){
-            res.json(notes);
-        });
+module.exports = (app) => {
+	fs.readFile("db/db.json", "utf8", (err, data) => {
+		if (err) throw err;
+		var notes = JSON.parse(data);
 
-        app.post("/api/notes",function(req, res) {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-        });
-        }
+		app.get("/api/notes", function (req, res) {
+			res.json(notes);
+		});
 
-        })
-    })
-}
+		app.post("/api/notes", function (req, res) {
+			let newNote = req.body;
+			notes.push(newNote);
+			updateDb();
+		});
 
-
-
+		app.get("/api/notes/:id", function (req, res) {
+			res.json(notes[req.params.id]);
+		});
+	});
+};
 
 app.get("/notes", (req, res) => {
 	res.sendfile(path.join(__dirname, "/public/notes.html"));
