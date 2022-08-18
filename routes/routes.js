540de4f30a3b19d5fedb482105e37107a06224fd
wjimh314 <wjimh314@gmail.com>
2022-08-18 14:46:54 -0400

    changed folder and deleted modules

---
+++ routes/routes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+module.exports = (app) => {
+	fs.readFile("db/db.json", "utf8", (err, data) => {
+		if (err) throw err;
+		var notes = JSON.parse(data);
+
+		app.get("/api/notes", function (req, res) {
+			res.json(notes);
+		});
+
+		app.post("/api/notes", function (req, res) {
+			let newNote = req.body;
+			notes.push(newNote);
+			updateDb();
+		});
+
+		app.get("/api/notes/:id", function (req, res) {
+			res.json(notes[req.params.id]);
+		});
+	});
+};
+
+app.get("/notes", (req, res) => {
+	res.sendfile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+function updateDb() {
+	fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
+		if (err) throw err;
+		return true;
+	});
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/routes")(app);
+
+app.listen(PORT, () => {
+	console.log("server working");
+});
