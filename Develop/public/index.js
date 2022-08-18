1edd5b25e5f969788b16a4b528031f064e7f178b
wjimh314 <wjimh314@gmail.com>
2022-08-18 13:08:10 -0400

    edit server.js

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,13 +1,3 @@
-app.get("/notes", (req, res) => {
-	res.sendfile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-	res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 let noteTitle;
 let noteText;
 let saveNoteBtn;
--- Develop/server.js
+++ Develop/server.js
@@ -6,3 +6,5 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("/routes/routes")(app);
